f065ab5026041a9b671aaa55f436a3c45f13750b
awheeler294 <awheeler294@gmail.com>
2016-10-17 08:12:32 -0600

    working on db insert

--- src/com/assignment5/DbInsert.java
+++ src/com/assignment5/DbInsert.java
@@ -4,11 +4,17 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 
 /**
  * Created by Andrew on 10/15/2016.
  */
 public class DbInsert {
+
+    private static String SCHEMA_NAME = "parent_child";
+    private static String DB_USER = "andrew";
+    private static String DB_PASS = "dangerwaffle";
+
     public DbInsert() {
 
     }
@@ -24,7 +30,7 @@ public class DbInsert {
             // create a connection (session) to the database
             //    connection string = "jdbc:progresql:<database URL:port number>/<database name>"
             //
-            Connection con = DriverManager.getConnection("jdbc:postgresql://ec2-52-88-214-84.us-west-2.compute.amazonaws.com:5432/cs452", "andrew", "dangerwaffle");
+            Connection con = DriverManager.getConnection("jdbc:postgresql://ec2-52-88-214-84.us-west-2.compute.amazonaws.com:5432/cs452", DB_USER, DB_PASS);
             if (null == con) {
                 System.out.println("failed to connect");
                 return;
@@ -34,9 +40,26 @@ public class DbInsert {
 
             List<String> columnNames = new ArrayList<>(Arrays.asList(tableData.get(0)));
 
-            String createStatement = "CREATE TABLE IF NOT EXIST parent_child." + tableName;
+            final String createTableQuery = "CREATE TABLE IF NOT EXIST " + SCHEMA_NAME + "." + tableName;
+            PreparedStatement createTableStatement = con.prepareStatement(createTableQuery);
+            createTableStatement.execute();
 
-            String
+            StringBuilder insertQueryBuilder = new StringBuilder("INSERT INTO " + SCHEMA_NAME + "." + tableName + "(");
+            StringBuilder insertValuesBuilder = new StringBuilder("VALUES (");
+
+            StringJoiner columnJoiner = new StringJoiner(", ");
+            StringJoiner valueJoiner = new StringJoiner(", ");
+
+            for (String columnName: columnNames) {
+                columnJoiner.add(columnName);
+                valueJoiner.add("?");
+            }
+
+            insertValuesBuilder.append(valueJoiner).append(")");
+            insertQueryBuilder.append(columnJoiner).append(")").append(insertValuesBuilder);
+
+            String insertQuery = insertQueryBuilder.toString();
+            PreparedStatement insertStatment = con.prepareStatement(insertQuery);
 
             for (int i = 1; i < tableData.size(); i++) {
 
